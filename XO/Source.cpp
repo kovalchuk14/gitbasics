7e934d0bb7a6d3cce087ae62fa1e5762cdb2a0a3
Student <Student@201-31>
2019-10-15 21:08:31 +0300

    .

--- XO/Source.cpp
+++ XO/Source.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 void PrintField(char Field[], const int n);
 void Move(char Field[], const int n);
+void Check(char Field[], const int n);
 
 void main()
 {
@@ -38,23 +39,64 @@ void Move(char Field[], const int n)
 {
 	char key;
 	static int h = 1;
-	//if (h = 10)return;
+	if (h == 10)
+	{
+		cout << "Draw" << endl;
+		return;
+	}
 		key = _getch();
 		if (key == 27)return;
 		//cout << (int)key << endl;
-		if (h % 2 == 1)
+		if (Field[key - 49] != 'X'&&Field[key - 49] != '0')
 		{
-			Field[key - 49] = 'X';
-			
-		}
-		if (h % 2==0)
-		{
-			Field[key - 49] = '0';
-		}
-			h++;
-		
-		system("cls");
-		PrintField(Field, n);
+			if (h % 2 == 1)
+			{
+				Field[key - 49] = 'X';
 
+			}
+			if (h % 2 == 0)
+			{
+				Field[key - 49] = '0';
+			}
+			h++;
+		}
+		system("cls");
+		Check(Field,n);
+		PrintField(Field, n);
+}
+void Check(char Field[], const int n)
+{
+	if (Field[0] == Field[1] && Field[0] == Field[2] && Field[0] != 0)
+	{
+		cout << "player " << Field[0] << " win" << endl;
+	}
+	if (Field[3] == Field[4] && Field[3] == Field[5]&& Field[3]!=0)
+	{
+		cout << "player " << Field[3] << " win" << endl;
+	}
+	if (Field[6] == Field[7] && Field[6] == Field[8] && Field[6] != 0)
+	{
+		cout << "player " << Field[6] << " win" << endl;
+	}
+	if (Field[0] == Field[3] && Field[0] == Field[6] && Field[0] != 0)
+	{
+		cout << "player " << Field[0] << " win" << endl;
+	}
+	if (Field[1] == Field[4] && Field[1] == Field[7] && Field[1] != 0)
+	{
+		cout << "player " << Field[1] << " win" << endl;
+	}
+	if (Field[2] == Field[5] && Field[2] == Field[8] && Field[2] != 0)
+	{
+		cout << "player " << Field[2] << " win" << endl;
+	}
+	if (Field[0] == Field[4] && Field[0] == Field[8] && Field[0] != 0)
+	{
+		cout << "player " << Field[0] << " win" << endl;
+	}
+	if (Field[2] == Field[4] && Field[2] == Field[6] && Field[2] != 0)
+	{
+		cout << "player " << Field[2] << " win" << endl;
+	}
 
 }
