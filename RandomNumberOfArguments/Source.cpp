8e19f4d56cf07e751b36953684f8a9ba707f2605
Student <Student@201-31>
2019-10-25 21:05:41 +0300

    /

---
+++ RandomNumberOfArguments/Source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+/*int sum(int n,int value, ...)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += *(&value + i);
+	}
+	return sum;
+}*/
+int sum(int value ...)
+{ 
+	int sum = 0;
+	int* u = &value;		
+	do
+	{ 
+		if (*u!= 0)
+		{
+			sum += *(u++);
+		}
+	} while (*u!=0);
+	return sum;
+}
+
+void main()
+{
+	cout << sum(3, 5, 8, 13, 21, 34,0);
+}
